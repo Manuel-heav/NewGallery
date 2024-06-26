b2158471496504a5566ac1c121b202a9536cb35b
manuel-heav <manuelheav32@gmail.com>
2024-06-26 18:17:44 +0300

    Did some parallel routing

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1e1855254c111aad181b8c9daf85a994@o4507498434461696.ingest.de.sentry.io/4507498448748624",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 
@@ -12,8 +13,10 @@ async function Images(){
     <div className="flex flex-wrap gap-4 justify-center">
       {images.map((image) => (
         <div key={image.id} className="flex w-48 flex-col">
-          <Image src={image.url} style={{objectFit: "contain"}} alt={image.name} width={480} height={480}/>
-          <div>{image.name}</div>
+          <Link href={`/photos/${image.id}`}>
+            <Image src={image.url} style={{objectFit: "contain"}} alt={image.name} width={480} height={480}/>
+          </Link>
+            <div>{image.name}</div>
         </div>
       ))}
     </div>
