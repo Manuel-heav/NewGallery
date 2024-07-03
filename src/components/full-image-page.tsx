5f39bbeeb2fecd4d23cbe8e78628e26426d6d45f
manuel-heav <manuelheav32@gmail.com>
2024-07-03 17:37:15 +0300

    Modifying modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,6 +3,13 @@ import { getImage } from "~/server/queries"
 export default async function FullPageImageView(props: {id: number}) {
     const image = await getImage(props.id)
     return (
-            <img src={image.url} className="w-96" />
+        <div className="flex w-full min-w-0 h-full">
+            <div className="flex-shrink flex justify-center items-center">
+                <img src={image.url} className="w-96 object-contain" />
+            </div>
+            <div className="w-48 flex-shrink-0 flex flex-col border-l">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
+        </div>
     )
 }
