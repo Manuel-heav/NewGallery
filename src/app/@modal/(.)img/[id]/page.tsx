967f1779e66d0b6c8c24f33db7ea0c200bba7291
manuel-heav <manuelheav32@gmail.com>
2024-07-03 17:21:31 +0300

    Made component for image view

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,13 +1,16 @@
+import FullPageImageView from "~/components/full-image-page";
 import { Modal } from "./Modal";
-import { getImage } from "~/server/queries";
 
-export default async function PhotoModal({
+export default function PhotoModal({
     params: {id: photoId},
 }: {
     params: {id: string};
 }){
     const idAsNum = Number(photoId);
     if(Number.isNaN(idAsNum)) throw new Error("Invalid Id");
-    const image = await getImage(idAsNum)
-    return <Modal><img src={image.url} alt={image.name} className="w-96"/></Modal>
+    return (
+    <Modal>
+        <FullPageImageView id={idAsNum} />
+    </Modal>
+    )
 }
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,16 +1,13 @@
-import { getImage } from "~/server/queries"
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default async function PhotoPage({
     params: {id: photoId},
 }: {
     params: {id: string}
 }) {
     const idAsNumber = Number(photoId)
     if(Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
-    const image = await getImage(idAsNumber)
     return (
-        <div>
-            <img src={image.url} className="w-96" />
-        </div>
+        <FullPageImageView id={idAsNumber} />
     )
 }
