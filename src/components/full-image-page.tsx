487b95a5e8a1918f9148bcc36e1aacd262239b4e
manuel-heav <manuelheav32@gmail.com>
2024-07-06 14:51:36 +0300

    Added delete button with server actions

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,10 @@
 import { clerkClient } from "@clerk/nextjs/server"
-import { getImage } from "~/server/queries"
+import { deleteImage, getImage } from "~/server/queries"
+import { Button } from "./ui/button"
 
 export default async function FullPageImageView(props: {id: number}) {
+    const idAsNumber = Number(props.id);
+    if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
     const image = await getImage(props.id)
 
     const uploaderInfo = await clerkClient.users.getUser(image.userId)
@@ -21,6 +24,15 @@ export default async function FullPageImageView(props: {id: number}) {
                 <div className="flex flex-col p-2">
                     <span>Created On:</span>
                     <span>{new Date(image.createdAt).toLocaleString()}</span>
+                </div>
+
+                <div className="flex flex-col p-2">
+                    <form action={async () => {
+                        "use server";
+                        await deleteImage(idAsNumber);
+                    }}>
+                        <Button type="submit" variant="destructive">Delete</Button>
+                    </form>
                 </div>
             </div>
         </div>
